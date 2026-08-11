365b44e42429fc6ab891fc6ef44de0de0d6b5369
KOKOU EDEM ADOGLI <81343117+kokouedemadogli@users.noreply.github.com>
2026-08-10 21:13:10 -0400

    Add Color theme definitions

---
+++ app/src/main/kotlin/com/dadeyes/parental/presentation/ui/theme/Color.kt
@@ -0,0 +1,34 @@
+package com.dadeyes.parental.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors
+val Primary = Color(0xFF6200EE)
+val PrimaryDark = Color(0xFF3700B3)
+val Secondary = Color(0xFF03DAC6)
+val SecondaryDark = Color(0xFF018786)
+val Tertiary = Color(0xFFFF6B6B)
+
+// Background & Surface
+val Background = Color(0xFFFAFAFA)
+val Surface = Color(0xFFFFFFFF)
+val SurfaceVariant = Color(0xFFF5F5F5)
+
+// Error
+val Error = Color(0xFFB3261E)
+val ErrorContainer = Color(0xFFF9DEDC)
+
+// Status Colors
+val Success = Color(0xFF4CAF50)
+val Warning = Color(0xFFFFC107)
+val Info = Color(0xFF2196F3)
+
+// Text Colors
+val OnBackground = Color(0xFF1F1F1F)
+val OnSurface = Color(0xFF1F1F1F)
+val OnPrimary = Color(0xFFFFFFFF)
+
+// Semantic Colors
+val BlockedRed = Color(0xFFE53935)
+val AllowedGreen = Color(0xFF43A047)
+val WarningOrange = Color(0xFFFB8C00)
